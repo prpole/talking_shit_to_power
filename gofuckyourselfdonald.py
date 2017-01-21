f82a793648f61d2ceceb1bed9ab78e7076e7e0e4
Phil Polefrone <pp2454@columbia.edu>
2017-01-20 20:08:05 -0500

    add timestamp

--- gofuckyourselfdonald.py
+++ gofuckyourselfdonald.py
@@ -13,4 +13,6 @@ auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 
-api.update_status(status='@readDonaldTrump Go fuck yourself. #TalkingShitToPower')
+nowtime = str(time.time())
+
+api.update_status(status='@readDonaldTrump Go fuck yourself. #TalkingShitToPower \n \n \n \n \n #'+nowtime)
