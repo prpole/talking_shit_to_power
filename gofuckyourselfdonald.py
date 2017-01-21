89d02b09f3d75976ccd48ae2c36b66a02fca16b6
Phil Polefrone <pp2454@columbia.edu>
2017-01-21 10:56:58 -0500

    fix access secret key

--- gofuckyourselfdonald.py
+++ gofuckyourselfdonald.py
@@ -9,6 +9,7 @@ from nltk.tokenize import RegexpTokenizer
 CONSUMER_KEY = 'YOUR_CONSUMER_KEY'#keep the quotes, replace this with your consumer key
 CONSUMER_SECRET = 'YOUR_CONSUMER_SECRET_KEY'#keep the quotes, replace this with your consumer secret key
 ACCESS_KEY = 'YOUR_ACCESS_KEY'#keep the quotes, replace this with your access token
+ACCESS_SECRET = 'YOUR_ACCESS_SECRET_KEY'#keep the quotes, replace this with your access token secret
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
